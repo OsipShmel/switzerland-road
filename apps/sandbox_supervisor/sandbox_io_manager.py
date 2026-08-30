d3034084ced2dad419d680368d8903e8a1bc8f69
banka-tushka <darsyslivaiva@gmail.com>
2026-08-30 10:50:33 +0000

    ещё больше асинхонщины

--- apps/sandbox_supervisor/sandbox_io_manager.py
+++ apps/sandbox_supervisor/sandbox_io_manager.py
@@ -10,80 +10,59 @@ from vls import VlsRegistry
 
 class SandboxIOManager:
 
-    def __init__(self, port: str) -> None:
-        
-        self._sandbox_target_url = f"http://sandboxd:{port}/target_zip"
-        self._sandbox_vls_regisry_rec = f"http://sandboxd:{port}/log_vls_registry"
-        self._sandbox_vls_reristry_up = f"http://sandboxd:{port}/init_vls_regisry"
-        self._sandbox_instruction_url = f"http://sandboxd:{port}/get_instruction"
+    def __init__(self, port: str, client: httpxAsyncClient) -> None:
+        self._client = client
+        base_url = f"http://sandboxd:{port}"
+        self._sandbox_target_url = f"{base_url}/target_zip"
+        self._sandbox_vls_regisry_rec = f"{base_url}/log_vls_registry"
+        self._sandbox_vls_registry_up = f"{base_url}/init_vls_regisry"
+        self._sandbox_instruction_url = f"{base_url}/get_instruction"
                 
     async def send_zip(self, zip_bytes: bytes, filename: str = "target.zip") -> bool:
-
-        async with httpx.AsyncClient() as cli:
-            files = {"file": (filename, zip_byte, "application/zip")}
-
-            try:
-                response = await cli.post(self._sandbox_url, files=files, timeout=60.0)
-                return response.is_success
-            
-            except httpx.HTTPStatusError as e:
-                print(f"HTTP error: {e.response.text}"}
-                return False
-
-            except httpx.RequestError as e:
-                print(f"transport reqest error:  {e}"}
-                return False
+        files = {"file": (filename, zip_bytes, "application/zip")}
+        try:
+            response = await self._client.post(self._sandbox_target_url, files=files, timeout=60.0)
+            return response.is_success  
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP error: {e.response.text}")
+            return False
+        except httpx.RequestError as e:
+            print(f"transport reqest error:  {e}")
+            return False
 
     async def receive_instruction(self) -> str:
+        try:    
+            response = await self._client.get(self._sandbox_instruction_url, timeout = 30.0)
+            response.raise_for_status()
+            return response.text
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP error: {e.response.text}")
+            return ""
+        except httpx.RequestError as e:
+            print(f"transport reqest error:  {e}")
+            return ""
         
-        async with httpx.AsyncClient() as cli:
-            
-            try:    
-                response = await cli.get(self._instruction_url, timeout = 30.0)
-                response.raise_for_status()
-                return response.text
-            
-            except httpx.HTTPStatusError as e:
-                print(f"HTTP error: {e.response.text}"}
-                return ""
-            
-            except httpx.RequestError as e:
-                print(f"transport reqest error:  {e}"}
-                return ""
-    
-    async def send_vls_regisrtry(self, vlsreg: VlsRegistry) -> bool:
-        
+    async def send_vls_registry(self, vlsreg: VlsRegistry) -> bool:
         vls_adapter = TypeAdapter(VlsRegistry)
         vls_json = vls_adapter.dump_python(vlsreg, mode="json")
-            
-        async with httpx.AsyncClient() as cli:
-            try:
-                response = await cli.post(self._sandbox_vls_reristry_up, json=vls_json, timeout=60.0)
-                return response.is_success
-            
-            except httpx.HTTPStatusError as e:
-                print(f"HTTP error: {e.response.text}"}
-                return False
-
-            except httpx.RequestError as e:
-                print(f"transport reqest error:  {e}"}
-                return False
-
-     async def receive_vls_regisrtry(self) -> VlsRegistry:
+        try:
+            response = await self._client.post(self._sandbox_vls_registry_up, json=vls_json, timeout=60.0)
+            return response.is_success
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP error: {e.response.text}")
+            return False
+        except httpx.RequestError as e:
+            print(f"transport reqest error:  {e}")
+            return False
         
+    async def receive_vls_regisrtry(self) -> VlsRegistry | None:
         vls_adapter = TypeAdapter(VlsRegistry)
-        vls_json = vls_adapter.dump_python(vlsreg, mode="json")
-            
-        async with httpx.AsyncClient() as cli:
-            try:
-                response = await cli.post(self._sandbox_vls_reristry_rec, timeout=60.0)
-                vlsreg = vls_adapter.validate_python(response.json())
-                return vlsreg
-            
-            except httpx.HTTPStatusError as e:
-                print(f"HTTP error: {e.response.text}"}
-                return False
-
-            except httpx.RequestError as e:
-                print(f"transport reqest error:  {e}"}
-                return False
+        try:
+            response = await self._client.post(self._sandbox_vls_registry_rec, timeout=60.0)
+            return vls_adapter.validate_python(response.json())
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP error: {e.response.text}")
+            return None
+        except httpx.RequestError as e:
+            print(f"transport reqest error:  {e}")
+            return None
--- apps/sandbox_supervisor/supervisor.py
+++ apps/sandbox_supervisor/supervisor.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
 import io
+import os
 import asyncio
-import zipfile
+import httpx
+import aiofiles.os
+from zipfile import ZipFile, ZIP_DEFLATED
 import docker
-from docker errors import NotFound
+from docker.errors import NotFound
 
 from VLSManager import VLSManager
 from sandbox_io_manager import SandboxIOManager
@@ -12,54 +15,73 @@ from sandbox_io_manager import SandboxIOManager
 class Supervisor:
 
     PORT = "1337"
+    MAX_RETRIES = 5
     
-    def __init__(self) -> None:
-
+    def __init__(self, client = httpx.AsyncClient) -> None:
         self._vlsmanager = VLSManager()
-        self._iomanager = SandboxIOManager(self.PORT)
-
+        self._iomanager = SandboxIOManager(self.PORT, self._client)
+        self._client = client
+        self._current_task = ""
+        
     async def start(self, target_link: str, vlsreg: VlsRegistry) -> None:
-
         await self._setup_target(target_link)
         print("starting vls transmission")
-
+        retries = 0
         while not await self._iomanager.send_vls_regisry(vlsreg):
+            retries += 1
             print("retrying vls transmission..")
-            await httpx.aio.sleep(5)
-
+            if retries >= self.MAX_RETIES:
+                print("failed to transfer")
+                return
+            await asyncio.sleep(5)
         print("vls successfully sent")
         self._insruct_sandbox("start")
-        
-    async def _setup_target(self, target_link: str) -> None:
 
-        target_zip_bytes = await self._get_target_zip(target_link)
+    async def _setup_target(self, target_link: str = "") -> None:
+        if target_link is not None:
+            target_zip_bytes = await self._get_target_zip(target_link)
+
+            def extract():
+                with ZipFile(io.BytesIO(target_zip_bytes)) as zip_ref:
+                    zip_ref.extractall("/tmp/target_app")
+
+            await asyncio.to_thread(extract)
+            print("Target successfully extracted")
+            
+        else:
+            print("starting target packing")
+
+            def pack():
+                with ZipFile(output_zip_path, 'w', ZIP_DEFLATED) as fzip:
+                    for root, dirs, files in os.walk("/tmp/target_app"):
+                        for file in files:
+                            file_path = os.path.join(root, file)
+                            archname = os.path.relpath(file_path, os.path.dirname("/tmp"))
+                            fzip.write(file_path, archname)
+
+            await asyncio.to_thread(pack)
+            
         print("starting target transmission")
-
+        retries = 0
         while not await self._iomanager.send_zip(target_zip_bytes):
+            retries += 1
+
             print("retrying target transmission..")
+            if retries >= self.MAX_RETIES:
+                print("failed to transfer")
+                return
             await httpx.aio.sleep(5)
 
         print("Target successfully sent")
+        return None
 
-        with zipfile.ZipFile(io.BytesIO(target_zip_bytes)) as zip_ref:
-            zip_ref.estractall("/tmp/target_app")
-
-        print("Target successfully extracted")
-    
-            
     async def _get_target_zip(self, target_link: str) -> bytes:
-        
-        async with httpx.AsyncClient() as cli:
-            resp = await cli.get(target_link)
-            resp.raise_for_status()
-            return resp.content
+        resp = await self._client.get(target_link)
+        resp.raise_for_status()
+        return resp.content
 
-    async def _port_checking(self, port: int) -> None:
+    async def _task_check(self, port: int) -> None:
         self._current_task = await self._iomanager.receive_instruction()
-
+        return None
         
-    def _send_to_sandbnox(self, args: str) -> None:
-        ...
-
-    
-    
+        
